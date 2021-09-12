2977de690df52e0b5936fc08b9a2bc34ee85807d
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-09-12 23:43:47 +0100

    Update mainn.js

--- live/mainn.js
+++ live/mainn.js
@@ -1,11 +1,6 @@
-
 function writeHTMLasJS(){
 	var HeNy007 = "";
-	HeNy007 += "  <!-- Top Bar --> ";
-	HeNy007 += "  <div id=\"top-bar\"> ";
-	HeNy007 += "   <h2>Aliwix Team - مجموعة أليويكس</h2> ";
-	HeNy007 += "  </div>";
-	HeNy007 += "  <ul id=\"container\"></ul> ";
+	HeNy007 += "<ul id=\"container\"></ul> ";
 	HeNy007 += "  </div>";
 	HeNy007 += "<center>";
 	HeNy007 += " <img class=\"animated-gif\" src=\"http://www.enigma2.net/wp-content/uploads/2018/07/iptv-ads.gif\" onclick=\"window.location='https://aliwixplay.netlify.app';\"><br>";
