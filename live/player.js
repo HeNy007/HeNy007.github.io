159bae366e0461cc6f5cc9da017fb1754f6069a6
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-09-13 11:26:59 +0100

    Update player.js

--- live/player.js
+++ live/player.js
@@ -3,7 +3,7 @@ jwplayer("my-video").setup({
    image: "https://scontent.ftun1-2.fna.fbcdn.net/v/t1.6435-9/fr/cp0/e15/q65/71318015_380217256223031_2019411863581753344_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=85a577&_nc_ohc=fEYS3VxbZ3EAX-wnGVD&_nc_ht=scontent.ftun1-2.fna&oh=1787679960ad14b11c44af14ecf58600&oe=61641E55",
    sharing: {code: '<iframe src="'+ window.location.href +'" frameborder="0" width="800" height="460"></iframe>',},
    link : {code: "'+ window.location.href +'",},   
-     title: 'HeNy007',autostart: "false",width: "97%",height: "560",
+     title: 'HeNy007',autostart: "false",width: "97%",height: "460",
      tag:"HeNy007",title:"aliwixplay.netlify.app",autostart:"false",stretching:"exactfit",autostretch:"false",
      logo:{position:"bottom-left",margin:"25",file:"https://github.com/HeNy007/HeNy007.github.io/raw/main/live/20210912_151611.png",height:"25"}, 
      flashplayer:"https://raw.githack.com/HeNy007/HeNy-Ben-Hamed/master/today-matches/jwplayer.flash.swf",tracks:{file:"thumbnails.vtt",kind:"thumbnails"} });
