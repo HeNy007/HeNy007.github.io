51d346e1fb4700ece447283a01398ea3e93f869e
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-09-28 13:44:26 +0100

    Create jadwal.js

---
+++ iframe/jadwal.js
@@ -0,0 +1,12 @@
+<script>
+function writeHTMLasJS(){
+	var HeNy007 = "";
+	HeNy007 += "<div class=\"main\">";
+	HeNy007 += " <div class=\"box\">";
+	HeNy007 += "<iframe src=\"https://live.koooragoal.com/\"></iframe>";
+
+	document.getElementById("js_html").innerHTML = HeNy007;
+}
+
+writeHTMLasJS();
+</script>
