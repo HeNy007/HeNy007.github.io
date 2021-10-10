159a7385d44fa23226b154a0b83606d84fbcf821
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-10-10 21:39:44 +0100

    Update mainn.js

--- live/mainn.js
+++ live/mainn.js
@@ -2,13 +2,15 @@ function writeHTMLasJS(){
 	var HeNy007 = "";
 	HeNy007 += "<ul id=\"container\"></ul> ";
 	HeNy007 += "  </div>";
-
-	HeNy007 += " <div>";
+        HeNy007 += "<br>";
+        HeNy007 += "<center><iframe data-aa=\"1810121\" src=\"//ad.a-ads.com/1810121?size=468x60\" style=\"width:468px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;\" ></iframe></center>\n";
+	HeNy007 += "<br>";
+        HeNy007 += " <div>";
 	HeNy007 += " <iframe src=\"https://live.koooragoal.com/\"></iframe>";
 	HeNy007 += "</div>";
         HeNy007 += "<br>";
-        
-
+        HeNy007 += "<center><iframe data-aa=\"1810121\" src=\"//ad.a-ads.com/1810121?size=468x60\" style=\"width:468px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;\" ></iframe></center>\n";
+       HeNy007 += "<br>";
 	document.getElementById("js_html").innerHTML = HeNy007;
 }
 
