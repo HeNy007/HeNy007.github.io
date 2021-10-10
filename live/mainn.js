9fa5c2941cbf14cec70b20ce5f7e794434f43e3a
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-10-10 21:30:42 +0100

    Update mainn.js

--- live/mainn.js
+++ live/mainn.js
@@ -2,18 +2,12 @@ function writeHTMLasJS(){
 	var HeNy007 = "";
 	HeNy007 += "<ul id=\"container\"></ul> ";
 	HeNy007 += "  </div>";
-	HeNy007 += "<center>\n";
-	HeNy007 += "<iframe src=\"https://ad2bitcoin.com/ad.php?ref=heny007&width=468\" marginwidth=\"0\" marginheight=\"0\" width=\"468\" height=\"60\" scrolling=\"no\" border=\"0\" frameborder=\"0\"></iframe>\n";
-	HeNy007 += "\t</center>\n";
-        HeNy007 += "<br>";
+
 	HeNy007 += " <div>";
 	HeNy007 += " <iframe src=\"https://live.koooragoal.com/\"></iframe>";
 	HeNy007 += "</div>";
         HeNy007 += "<br>";
-        HeNy007 += "<center>\n";
-	HeNy007 += "<iframe src=\"https://ad2bitcoin.com/ad.php?ref=heny007&width=468\" marginwidth=\"0\" marginheight=\"0\" width=\"468\" height=\"60\" scrolling=\"no\" border=\"0\" frameborder=\"0\"></iframe>\n";
-	HeNy007 += "\t</center>\n";
-        HeNy007 += "</center>";
+        
 
 	document.getElementById("js_html").innerHTML = HeNy007;
 }
