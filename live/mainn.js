82bc7c2275392bc4e1d93907475a46a34511b210
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-10-10 21:11:41 +0100

    Update mainn.js

--- live/mainn.js
+++ live/mainn.js
@@ -2,15 +2,18 @@ function writeHTMLasJS(){
 	var HeNy007 = "";
 	HeNy007 += "<ul id=\"container\"></ul> ";
 	HeNy007 += "  </div>";
-	HeNy007 += "<center>";
-	HeNy007 += " <img class=\"animated-gif\" src=\"http://www.enigma2.net/wp-content/uploads/2018/07/iptv-ads.gif\" onclick=\"window.location='https://aliwixplay.netlify.app';\"><br>";
-	HeNy007 += "<br>";
+	HeNy007 += "<center>\n";
+	HeNy007 += "<iframe src=\"https://ad2bitcoin.com/ad.php?ref=heny007&width=468\" marginwidth=\"0\" marginheight=\"0\" width=\"468\" height=\"60\" scrolling=\"no\" border=\"0\" frameborder=\"0\"></iframe>\n";
+	HeNy007 += "\t</center>\n";
+        HeNy007 += "<br>";
 	HeNy007 += " <div>";
 	HeNy007 += " <iframe src=\"https://live.koooragoal.com/\"></iframe>";
 	HeNy007 += "</div>";
         HeNy007 += "<br>";
-        HeNy007 += " <img class=\"animated-gif\" src=\"http://d1pr3sk5xr0dce.cloudfront.net/banners/728x90_ani.gif\" onclick=\"window.location='https://adcash.myadcash.com/register?prmid=J35D6TGFBN';\"><br>";
-	HeNy007 += "</center>";
+        HeNy007 += "<center>\n";
+	HeNy007 += "<iframe src=\"https://ad2bitcoin.com/ad.php?ref=heny007&width=468\" marginwidth=\"0\" marginheight=\"0\" width=\"468\" height=\"60\" scrolling=\"no\" border=\"0\" frameborder=\"0\"></iframe>\n";
+	HeNy007 += "\t</center>\n";
+        HeNy007 += "</center>";
 
 	document.getElementById("js_html").innerHTML = HeNy007;
 }
