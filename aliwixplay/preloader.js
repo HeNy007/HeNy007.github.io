b24c4fa62717b7c3d7430268442c9dcc5676247a
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-02-16 22:09:19 +0100

    Create preloader.js

---
+++ aliwixplay/preloader.js
@@ -0,0 +1,4 @@
+$(window).on("load", function() {
+ $(".preloader").fadeOut("slow")
+ })
+document.write("<style>\r\n  .preloader {\r\n  position: fixed;\r\n  left: 0;\r\n  top: 0;\r\n  z-index: 999;\r\n  width: 100%;\r\n  height: 100%;\r\n  background: #fff url(\"https:\/\/heny007.github.io\/loading-45.gif\");\r\n  background-repeat: no-repeat;\r\n  background-position: center center;\r\n  background-size: contain;\r\n  background-attachment: fixed;\r\n  }\r\n  <\/style>");
